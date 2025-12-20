4ca08f9b569c042cfd71a444300adb5b889fcef4
Raika <dgnika@gmail.com>
2025-12-20 22:06:25 +0100

    Setup the project with all dependencies but threejs is not working damnit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import * as THREE from 'three'
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(300, 300)
+renderer.setClearColor(0xff0000)
+
+document.body.appendChild(renderer.domElement)
+
+// import * as THREE from 'three'
+
+// const renderer = new THREE.WebGLRenderer()
+// renderer.setSize(300, 300)
+// renderer.setClearColor(0xff0000)
+
+// document.body.appendChild(renderer.domElement)
