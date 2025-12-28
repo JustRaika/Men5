2df23d8af93c3203f347df41eb296b4a02d3cc95
Jan Schwegler <schwegler.jan@gmail.com>
2025-12-29 00:44:37 +0100

    add shader normal

---
+++ src/shaders/materials/normal/material.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+import vertexShader from './normal.vert?raw';
+import fragmentShader from './normal.frag?raw';
+import { sharedUniforms } from '../../uniforms.js';
+
+export function createNormalMaterial() {
+    return new THREE.ShaderMaterial({
+        name: 'Normal',
+        uniforms: { 
+            ...sharedUniforms,
+            // Custom uniforms
+        },
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    });
+}
