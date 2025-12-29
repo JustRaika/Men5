7f0cc08e0d15f9c612fbff4429be319da8fcabb4
Jan Schwegler <schwegler.jan@gmail.com>
2025-12-29 18:36:34 +0100

    create shader preset

---
+++ src/shaders/materials/_preset/material.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+import vertexShader from './vertex.glsl?raw';
+import fragmentShader from './fragment.glsl?raw';
+import { sharedUniforms } from '../../uniforms.js';
+
+export function createPresetMaterial() {    // change function name
+    return new THREE.ShaderMaterial({
+        name: 'Preset',                     // change material name
+        uniforms: { 
+            ...sharedUniforms,
+            // Custom uniforms
+        },
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    });
+}
