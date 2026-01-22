24420e1d8c885e9d3479a7b482e96682fd0bb41e
Jan Schwegler <schwegler.jan@gmail.com>
2026-01-22 17:44:01 +0100

    shader fbm fix name

--- src/shaders/materials/09_noise_fbm/material.js
+++ src/shaders/materials/09_noise_fbm/material.js
@@ -5,7 +5,7 @@ import { sharedUniforms } from '../../uniforms.js';
 
 export function createNoise2Material() {    // change function name
     return new THREE.ShaderMaterial({
-        name: 'Fractial Brownian motion',        // change material name
+        name: 'Fractal Brownian Motion',        // change material name
         userData: {                              // change material description
             description: `
                 This shader showcases Fractal Brownian Motion (fBM). In our case, this means layering the same noise function multiple times (called octaves) to create richer and more detailed patterns. Our fBM function directly calls the Simplex 3D noise function. To use other noise types, the same structure can be reused by copying the code and calling a different noise function inside the loop.
